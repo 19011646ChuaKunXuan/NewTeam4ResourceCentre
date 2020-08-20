0c03081e550dfbdf046b042c5f91487476a24bc5
19017935-KohSoonChuan <19017935@myrp.edu.sg>
2020-08-20 18:12:09 +0800

    added customer junit - kun xuan

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -22,6 +22,12 @@ public class C206_CaseStudyTest {
 
 	private ArrayList<Procedure> procedureList;
 	
+	//Customer for test - kun xuan
+	public Customer a;
+	public Customer b;
+
+	private ArrayList<Customer> customerList;
+	
 	
 
 	@Before
@@ -40,6 +46,12 @@ public class C206_CaseStudyTest {
 		pr3 = new Procedure("If purchase happend more than 30 days ago, item cannot be returned", "return");
 
 		procedureList = new ArrayList<Procedure>();
+		
+		// preparing test data for Customer - kun xuan
+		Customer a = new Customer("Kun Xuan", 98765432);
+		Customer b = new Customer("Soon Chuan", 12345678);
+		
+		customerList = new ArrayList<Customer>();
 
 	}
 
@@ -59,11 +71,18 @@ public class C206_CaseStudyTest {
 		pr2 = null;
 		pr3 = null;
 		procedureList = null;
+		
+		// for Customer - kun xuan
+		a = null;
+		b = null;
+		customerList = null;
+		
+		
 	}
 
 
 
-	@Test
+	@Test // - soon chuan
 	// Tests for adding transaction - soon chuan
 	public void addTransactionTest() {
 		// normal: transactions should be added into its dedicated list
@@ -76,7 +95,8 @@ public class C206_CaseStudyTest {
 		assertFalse(tt2.equals(transactionList.get(0)));
 
 	}
-
+	
+	@Test // - soon chuan
 	// Tests for viewing/retrieving transactions - soon chuan
 	public void viewTransactionTest() {
 		// normal: all existing transaction records should be displayed
@@ -90,7 +110,8 @@ public class C206_CaseStudyTest {
 		assertNotNull("Test if there is a valid transaction arraylist to retrieve transactions", transactionList);
 
 	}
-
+	
+	@Test // - soon chuan
 	// tests for archiving transactions - soon chuan
 	public void archiveTransactionTest() {
 		// normal: archived transactions should no longer show up in transactionList
@@ -100,7 +121,8 @@ public class C206_CaseStudyTest {
 		assertNotSame(currentSize, transactionList.size());
 
 	}
-
+	
+	@Test // - Leonard
 	public void addProcedureTest() {
 
 		// Check for correctly added procedure - Leonard
@@ -117,7 +139,8 @@ public class C206_CaseStudyTest {
 		assertSame("Check that Procedure is added in the same position", pr1, procedureList.get(0));
 
 	}
-
+	
+	@Test // - Leonard
 	public void deleteProcedureTest() {
 
 		// Check if procedure is deleted from the list properly - Leonard
@@ -136,13 +159,54 @@ public class C206_CaseStudyTest {
 		assertEquals("Check that procedure list has only 1 procedure", 1, procedureList.size());
 
 	}
-
+	
+	@Test // - Leonard
 	public void viewProcedureTest() {
 		// Check if every procedure is added into the list correctly - Leonard
 		C206_CaseStudy.addProcedure(procedureList, pr1);
 		C206_CaseStudy.addProcedure(procedureList, pr2);
 		assertEquals("Check that procedure list has 2 procedures", 2, procedureList.size());
 
+	}
+	
+	@Test // - kun xuan
+	public void addcustomerInfo() { 
+		//When the test case is empty, add a customer name and test if there is only 1 item in the array list.(normal)
+		C206_CaseStudy.addcustomerInfo(customerList, a);
+		assertEquals("Test if customer arraylist size is 1?", 1, customerList.size());
+		//boundary
+		//Test if the array list is not null to add new item. (boundary)
+		assertNotNull("Check if there is valid customer arraylist add to", customerList);
+		//After added, the customer information should not be able to add again. (Error)
+		C206_CaseStudy.addcustomerInfo(customerList, b);
+		assertNotEquals("Test if customer can be added again", 0, customerList);
+		
+	}
+	
+	@Test // - kun xuan
+	public void viewcustomerInfo() {
+		//The list is empty at first but able to view the information after adding 1 (Normal)
+		C206_CaseStudy.addcustomerInfo(customerList,a);
+		assertNotEquals("Test if there is any customer information arraylist that can be viewed", 0, customerList);
+		//Customer information list cannot be null before viewing the customer information (Boundary)
+		assertNotNull("Check if there is valid customer arraylist to add to", customerList);
+		//if the item does not exist, it should not be viewed (Error)	
+		String testoutput = "";
+		assertEquals("Test if customer information can be viewed if it is not available", testoutput, C206_CaseStudy.retrieveAllCustomerInformation(customerList));
+	
+	}
+	
+	@Test // - kun xuan
+	public void deletecustomerInfo() {
+		// Test if when customer information is deleted, the size of customerList will be reduced by 1 (normal)
+		C206_CaseStudy.deletecustomerInfo(customerList);
+		assertEquals("Test if customer arraylist size is 0?", 0, customerList.size());
+		// customer list cannot be null when deleting a product (boundary) 
+		assertNotNull("Check if there is any customer Information to be deleted", customerList);
+		// when deleted, it should no longer be on the list (error)
+		C206_CaseStudy.deletecustomerInfo(customerList);
+		assertNotEquals("Test if there if the customer's Information is still in the customerList", 0, customerList);
+	
 	}
 
 }
